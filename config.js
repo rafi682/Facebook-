0870d2efc1a52e3f3723a2bf72e090b3858ac1e6
rafi682 <rafinoton@gmail.com>
2025-07-25 14:54:39 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    dashboardPassword: "your-password",
-    logType: "json",
+    dashboardPassword: "rafi2323",
+    logType: "tgBot",
     note: "Change this to 'mongodb', 'tgBot' or 'json' as needed",
     timezone: "Asia/Dhaka",
     bot: {
-        token: "telegram_bot_token",
-        id: "telegram_chat_id",
+        token: "7917033015:AAHWoRHmLPfota52nDe6zYI4hqc188wAbJA",
+        id: "7601907182",
         note: "Add telegram bot token and chatid for telegram log."
     },
     database: {
